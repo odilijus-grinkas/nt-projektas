cb0225433bc937c38868d8b0b1c8fcc4ff5ed41d
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-11-24 14:57:07 +0200

    Sukurta galerija

---
+++ klases/galerija.js
@@ -0,0 +1,21 @@
+class Galerija{
+  constructor(url1="Nera Nuotrauku", url2=null, url3=null){
+    this.url1 = url1;
+    this.url2 = url2;
+    this.url3 = url3;
+  };
+  isvedimasKataloguiNT(){
+    let img = document.createElement("img");
+    img.src=this.url1;
+    return img
+  }
+  isvedimasKataloguiAgento(){
+    return -1;
+  }
+  galerijaNT(){
+    return -1;
+  }
+  isvdeimasProfilis(){
+    return -1;
+  }
+}
