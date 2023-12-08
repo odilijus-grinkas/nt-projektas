55271792217126da8d32e7f2e526601e13dfda98
odilijus-grinkas <odilijus.grinkas@stud.ktmc.lt>
2023-12-08 12:36:59 +0200

    again edit

--- created_objects/objektai.js
+++ created_objects/objektai.js
@@ -3,13 +3,11 @@
 import { komerciniaiObjektai } from "./komerciniaiObjektai.js";
 import { namai } from "./namaiObjektai.js";
 import { sklypai } from "./sklypaiObjektai.js"
-import { butaiObjektai } from "./butaiObjektai.js";
 
 const sujungtiObjektai = [];
 sujungtiObjektai.push(namai);
 sujungtiObjektai.push(komerciniaiObjektai);
 sujungtiObjektai.push(sklypai);
-sujungtiObjektai.push(butaiObjektai);
 
 // Visi sukurti objektu masyvai bus istraukti is tu masyvu: [[obj1],[obj2]] => [obj,obj2]
 const objektai = sujungtiObjektai.flat(1);
