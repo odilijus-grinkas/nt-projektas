aa1cdb93a982513bb126737ab5fde3514f1d2034
DominykasKupsys <dominykas.kupsys@stud.ktmc.lt>
2023-12-13 19:57:31 +0200

    pakeiciau namas isvedima

--- created_objects/namaiObjektai.js
+++ created_objects/namaiObjektai.js
@@ -1,78 +1,6 @@
 import { Namas, NamasNuoma } from "../klases/namas.js";
 import { agentai } from "./agentai.js";
-import { Adresas } from "../klases/adresas.js";
 import { regionai } from "./regionai.js";
-
-let adresasobject1 = {
-  regionas: regionai[0].nt(),
-  rajonas: "Vilniaus rajono savivaldybė",
-  miestas: "Vilnius",
-  gatve: "Gėlių g. 59"
-};
-let adresasobject2 = {
-  regionas: regionai[1].nt(),
-  rajonas: "Klaipėdos rajono savivaldybė",
-  miestas: "Klaipėda",
-  gatve: "Medžių g. 55"
-};
-let adresasobject3 = {
-  regionas: regionai[1].nt(),
-  rajonas: "Klaipėdos rajono savivaldybė",
-  miestas: "Klaipėda",
-  gatve: "Rožių g. 122"
-};
-let adresasobject4 = {
-  regionas: regionai[2].nt(),
-  rajonas: "Panevėžio miesto savivaldybė",
-  miestas: "Panevėžys",
-  gatve: "Žirgų g. 25"
-};
-let adresasobject5 = {
-  regionas: regionai[3].nt(),
-  rajonas: "Šiaulių miesto savivaldybė",
-  miestas: "Šiaulių",
-  gatve: "Kopūstų g. 5"
-};
-let adresasobject6 = {
-  regionas: regionai[4].nt(),
-  rajonas: "Tauragės rajono savivaldybė",
-  miestas: "Tauragė",
-  gatve: "Lenkų g. 45"
-};
-let adresasobject7 = {
-  regionas: regionai[5].nt(),
-  rajonas: "Kauno miesto savivaldybė",
-  miestas: "Kaunas",
-  gatve: "Malūnų g. 60"
-};
-let adresasobject8 = {
-  regionas: regionai[1].nt(),
-  rajonas: "Klaipėdos rajono savivaldybė",
-  miestas: "Klaipėda",
-  gatve: "Puodų g. 75"
-};
-let adresasobject9 = {
-  regionas: regionai[7].nt(),
-  rajonas: "Utenos rajono savivaldybė",
-  miestas: "Utena",
-  gatve: "Tulpių g. 89"
-};
-let adresasobject10 = {
-  regionas: regionai[5].nt(),
-  rajonas: "Kauno rajono savivaldybė",
-  miestas: "Kaunas",
-  gatve: "Meilės g. 5"
-};
-let adresas1 = new Adresas(adresasobject1)
-let adresas2 = new Adresas(adresasobject2)
-let adresas3 = new Adresas(adresasobject3)
-let adresas4 = new Adresas(adresasobject4)
-let adresas5 = new Adresas(adresasobject5)
-let adresas6 = new Adresas(adresasobject6)
-let adresas7 = new Adresas(adresasobject7)
-let adresas8 = new Adresas(adresasobject8)
-let adresas9 = new Adresas(adresasobject9)
-let adresas10 = new Adresas(adresasobject10)
 
 let namai = [
   new Namas({
@@ -82,7 +10,12 @@ let namai = [
         Vos už kelių minučių pėsčiomis yra Medžiakalnio geomorfologinis draustinis,
         aplinkui gyvenvietę yra net keli ežerai. Puikus pasirinkimas norintiems gyventi ramioje gamtos apsuptyje,
         kartu nesunkiai pasiekiant miesto centrą. Namas yra įrengtas pagal A++ klasės reikalavimus.`,
-    adresas: adresas1,
+    adresas: {
+      regionas: regionai[0].nt(),
+      rajonas: "Vilniaus rajono savivaldybė",
+      miestas: "Vilnius",
+      gatve: "Gėlių g. 59",
+    },
     galerija: [
       "https://aruodas-img.dgn.lt/object_62_114207926/vilniaus-r-sav-geleziu-k-vanagu-g.jpg",
       "https://aruodas-img.dgn.lt/object_62_114207924/vilniaus-r-sav-geleziu-k-vanagu-g.jpg",
@@ -99,7 +32,12 @@ let namai = [
     kaina: 261160,
     aprasymas: `Parduodami DU A++ energinės klasės namai, pro kurių langus atsiveria miško arba upės vaizdai.
     Abu namai turi dideles, net 40 kv. m. stogines, erdvius kiemus.`,
-    adresas: adresas2,
+    adresas: {
+      regionas: regionai[1].nt(),
+      rajonas: "Klaipėdos rajono savivaldybė",
+      miestas: "Klaipėda",
+      gatve: "Medžių g. 55",
+    },
     galerija: [
       "https://aruodas-img.dgn.lt/object_62_114259076/kauno-r-sav-karmelavos-ii-k-pilies-g.jpg",
       "https://aruodas-img.dgn.lt/object_62_114259032/kauno-r-sav-karmelavos-ii-k-pilies-g.jpg",
@@ -116,7 +54,12 @@ let namai = [
     kaina: 2680000,
     aprasymas: `IŠSKIRTINIS PASIŪLYMAS - UNIKALI GALIMYBĖ, IEŠKANTIEMS PRABANGAUS, ERDVAUS, KOKYBIŠKO IR PATOGAUS GYVENIMO PAJŪRYJE -
      PARDUODAMI NAMAI KARKLĖJE.`,
-    adresas: adresas3,
+    adresas: {
+      regionas: regionai[1].nt(),
+      rajonas: "Klaipėdos rajono savivaldybė",
+      miestas: "Klaipėda",
+      gatve: "Rožių g. 122",
+    },
     galerija: [
       "https://static.capital.lt/files/39/photos/003/084/670/files/for_export.jpg",
       "https://static.capital.lt/files/39/photos/003/084/685/files/for_export.jpg",
@@ -135,7 +78,12 @@ let namai = [
     Šalia namų, tame pačiame kvartalėlyje veikia valstybinis lopšelis - darželis,
      suformuotas mokyklinio autobusiuko maršrutas vyresniems vaikams, jau lankantiems mokyklą.
     Gatvelėje TIK INDIVIDUALŪS NAMAI, JOKIŲ KOTEDŽŲ!`,
-    adresas: adresas4,
+    adresas: {
+      regionas: regionai[2].nt(),
+      rajonas: "Panevėžio miesto savivaldybė",
+      miestas: "Panevėžys",
+      gatve: "Žirgų g. 25",
+    },
     galerija: [
       "https://static.capital.lt/files//photos/003/950/486/files/for_export.jpg",
       "https://static.capital.lt/files//photos/003/950/469/files/for_export.jpg",
@@ -153,7 +101,12 @@ let namai = [
     aprasymas: `STRATEGIŠKAI PATOGIOJE VIETOJE PARDUODAMAS ERDVUS 130 KV.M NAMAS, SLYVŲ GATVĖJE, GIRAITĖJE !
   Pilnai įrengtas, funkcionalus ir patogiai suprojektuotas dviaukštis gyvenamasis namas, sublokuotas per garažą,
    kuriam priklauso 6a. sklypas.`,
-    adresas: adresas5,
+    adresas: {
+      regionas: regionai[3].nt(),
+      rajonas: "Šiaulių miesto savivaldybė",
+      miestas: "Šiaulių",
+      gatve: "Kopūstų g. 5",
+    },
     galerija: [
       "https://static.capital.lt/files/39/photos/003/950/169/files/for_export.jpg",
       "https://static.capital.lt/files/39/photos/003/950/170/files/for_export.jpg",
@@ -172,7 +125,10 @@ let namai = [
         A garage's price - 40 000 Eur.
         There is a walk just 10 minutes away from the townhouse to the historic centre of the capital and major business centres.
         In addition, the complex is located in a quiet area near the main city park.`,
-    adresas: adresas6,
+    adresas: { regionas: regionai[4].nt(),
+      rajonas: "Tauragės rajono savivaldybė",
+      miestas: "Tauragė",
+      gatve: "Lenkų g. 45",},
     galerija: [
       "https://aruodas-img.dgn.lt/object_62_112775729/vilnius-senamiestis-pylimo-g.jpg",
       "https://aruodas-img.dgn.lt/object_62_112775731/vilnius-senamiestis-pylimo-g.jpg",
@@ -188,7 +144,12 @@ let namai = [
     id: 7,
     kaina: 1238,
     aprasymas: `Puikui namas, labai gražioje vietoja`,
-    adresas: adresas7,
+    adresas: {
+      regionas: regionai[5].nt(),
+      rajonas: "Kauno miesto savivaldybė",
+      miestas: "Kaunas",
+      gatve: "Malūnų g. 60",
+    },
     galerija: [
       "https://aruodas-img.dgn.lt/object_62_110716589/kauno-r-sav-uzliedziu-k-aitvaru-g.jpg",
       "https://aruodas-img.dgn.lt/object_62_110716615/kauno-r-sav-uzliedziu-k-aitvaru-g.jpg",
@@ -205,7 +166,12 @@ let namai = [
     kaina: 4900,
     aprasymas: `Erdvus ir tuo pačiu jaukus prabangiai įrengtas namas laukia naujųjų gyventojų.
      Pirmame namo aukšte erdvi svetainė su židiniu ir išėjimu į terasą, šalia - jaukus valgomasis ir funkcionali virtuvė.`,
-    adresas: adresas8,
+    adresas: {
+      regionas: regionai[1].nt(),
+      rajonas: "Klaipėdos rajono savivaldybė",
+      miestas: "Klaipėda",
+      gatve: "Puodų g. 75",
+    },
     galerija: [
       "https://static.capital.lt/files/39/photos/003/484/339/files/for_export.jpg",
       "https://static.capital.lt/files/39/photos/003/484/381/files/for_export.jpg",
@@ -223,7 +189,12 @@ let namai = [
     aprasymas: `Nuomojamas 78 kv.m namas esantis išskirtinėje ir ramioje vietoje,
      kur galėsite džiaugtis gamtos ramybe, būdami visai netoli nuo Vilniaus miesto centro.
     `,
-    adresas: adresas9,
+    adresas: {
+      regionas: regionai[7].nt(),
+      rajonas: "Utenos rajono savivaldybė",
+      miestas: "Utena",
+      gatve: "Tulpių g. 89",
+    },
     galerija: [
       "https://static.capital.lt/files/39/photos/003/919/887/files/for_export.jpg",
       "https://static.capital.lt/files/39/photos/003/919/596/files/for_export.jpg",
@@ -241,7 +212,12 @@ let namai = [
     aprasymas: `PUIKUS PASIŪLYMAS ŠEIMAI, IEŠKANČIAI NAMŲ RAMIOJE, MAŽAI NUTOLUSIOJE NUO VILNIAUS MIESTO CENTRO VIETOJE.
      SKONINGAI ĮRENGTAS, PATOGAUS IŠPLANAVIMO, ERDVUS KETURIŲ KAMBARIŲ NAMAS IEŠKO NAUJŲ ŠEIMININKŲ!
   `,
-    adresas: adresas10,
+    adresas:  {
+      regionas: regionai[5].nt(),
+      rajonas: "Kauno rajono savivaldybė",
+      miestas: "Kaunas",
+      gatve: "Meilės g. 5",
+    },
     galerija: [
       "https://static.capital.lt/files//photos/003/940/448/files/for_export.jpg",
       "https://static.capital.lt/files//photos/003/940/450/files/for_export.jpg",
